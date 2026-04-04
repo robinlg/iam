edefe3cadcbcb8765ad43c1f8f9fe6416a4d08f1
Robin Liu <robinliu27@163.com>
2026-04-04 15:25:51 +0800

    feat: add swagger

---
+++ api/swagger/docs/doc.go
@@ -0,0 +1,35 @@
+// Copyright 2025 Robin Liu <robinliu27@163.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+// Package docs IAM API Server API.
+//
+// Identity and Access Management System.
+//
+//	    Schemes: http, https
+//	    Host: iam.api.robinlg.com
+//	    BasePath: /v1
+//	    Version: 1.0.0
+//		   License: MIT https://opensource.org/licenses/MIT
+//		   Contact: Robin Liu <robinliu27@163.com> http://robinlg.com
+//
+//	    Consumes:
+//	    - application/json
+//
+//	    Produces:
+//	    - application/json
+//
+//	    Security:
+//	    - basic
+//	    - api_key
+//
+//	   SecurityDefinitions:
+//	   basic:
+//	     type: basic
+//	   api_key:
+//	     type: apiKey
+//	     name: Authorization
+//	     in: header
+//
+// swagger:meta
+package docs
