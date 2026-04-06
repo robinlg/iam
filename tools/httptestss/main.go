0caabcf1011f37ea21376fb8912e7f44bcf897a2
Robin Liu <robinliu27@163.com>
2026-04-06 14:03:24 +0800

    feat: add test

---
+++ tools/httptestss/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		message := `{"status":"ok"}`
+		fmt.Fprint(w, message)
+	})
+
+	addr := ":6667"
+	fmt.Printf("Serving http service on %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
