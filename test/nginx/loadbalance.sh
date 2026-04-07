b0a438afc3aa9b46cd98cc5628753cfb0ab4dcb0
Robin Liu <robinliu27@163.com>
2026-04-07 09:03:59 +0800

    feat: add test

---
+++ test/nginx/loadbalance.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Copyright 2025 Robin Liu <robinliu27@163.com>. All rights reserved.
+# Use of this source code is governed by a MIT style
+# license that can be found in the LICENSE file.
+
+
+for n in $(seq 1 1 10)
+do
+  nohup curl http://iam.api.robinlg.com/healthz &>/dev/null &
+done
